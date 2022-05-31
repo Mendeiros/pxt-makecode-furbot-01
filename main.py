f29b245c96d6c98065d440c2a5b96f8da98f5cf2
Gabriel Mendes Medeiros <94714375+Mendeiros@users.noreply.github.com>
2022-05-31 17:16:57 -0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 leituraImagem = ""
 valoresIRs = 0
 lerCarta = False
-mensagemFoto = "{\"FurbotText\": \"TirarFoto\"}"
+mensagemFoto = "{\"FurbotText\": \"tirar foto\"}"
 direçãoAtual = 0            #(N=0, O=1, S=2, L=3), VIRARDIREITA = +1, VIRARESQUERDA = -1;
 andarReto = True
 sonar = 0
@@ -51,85 +51,77 @@ def on_forever():
             basic.pause(1640)
             leituraImagem = serial.read_string()
             leituraImagem.to_lower_case()
-        if leituraImagem.includes("{\"furbottext\": \"virardireita\"}"):
-            lerCarta = False
-            turtleBit.run(DIR.RUN_BACK, 55)
-            basic.pause(280)
-            turtleBit.run(DIR.TURN_RIGHT, 55)
-            direçãoAtual += 1
-            if direçãoAtual > 3:
-                direçãoAtual = 0
-            basic.pause(1062)
-            turtleBit.run(DIR.RUN_FORWARD, 55)
-            basic.pause(280)
-            turtleBit.state(MotorState.STOP)
-            lerCarta = True
-        elif leituraImagem.includes("{\"furbottext\": \"viraresquerda\"}"):
-            lerCarta = False
-            turtleBit.run(DIR.RUN_BACK, 55)
-            basic.pause(280)
-            turtleBit.run(DIR.TURN_LEFT, 55)
-            direçãoAtual += -1
-            if direçãoAtual < 0:
-                direçãoAtual = 3
-            basic.pause(1145)
-            turtleBit.run(DIR.RUN_FORWARD, 55)
-            basic.pause(280)
-            turtleBit.state(MotorState.STOP)
-            lerCarta = True
-        elif leituraImagem.includes("{\"furbottext\": \"andarnorte\"}") and direçãoAtual == 0:
-            lerCarta = False
-            andarReto = True
-        elif leituraImagem.includes("{\"furbottext\": \"andaroeste\"}") and direçãoAtual == 1:
-            lerCarta = False
-            andarReto = True
-        elif leituraImagem.includes("{\"furbottext\": \"andarsul\"}") and direçãoAtual == 2:
-            lerCarta = False
-            andarReto = True
-        elif leituraImagem.includes("{\"furbottext\": \"andarleste\"}") and direçãoAtual == 3:
-            lerCarta = False
-            andarReto = True
-        else:
-            basic.pause(120)
-            basic.clear_screen()
-            basic.show_leds("""
+            if leituraImagem.includes("{\"furbottext\": \"virardireita\"}"):
+                lerCarta = False
+                turtleBit.run(DIR.RUN_BACK, 55)
+                basic.pause(280)
+                turtleBit.run(DIR.TURN_RIGHT, 55)
+                direçãoAtual += 1
+                if direçãoAtual > 3:
+                    direçãoAtual = 0
+                basic.pause(1062)
+                turtleBit.run(DIR.RUN_FORWARD, 55)
+                basic.pause(280)
+                turtleBit.state(MotorState.STOP)
+                lerCarta = True
+            elif leituraImagem.includes("{\"furbottext\": \"viraresquerda\"}"):
+                lerCarta = False
+                turtleBit.run(DIR.RUN_BACK, 55)
+                basic.pause(280)
+                turtleBit.run(DIR.TURN_LEFT, 55)
+                direçãoAtual += -1
+                if direçãoAtual < 0:
+                    direçãoAtual = 3
+                basic.pause(1145)
+                turtleBit.run(DIR.RUN_FORWARD, 55)
+                basic.pause(280)
+                turtleBit.state(MotorState.STOP)
+                lerCarta = True
+            elif leituraImagem.includes("{\"furbottext\": \"andarnorte\"}") and direçãoAtual == 0:
+                lerCarta = False
+                andarReto = True
+            elif leituraImagem.includes("{\"furbottext\": \"andaroeste\"}") and direçãoAtual == 1:
+                lerCarta = False
+                andarReto = True
+            elif leituraImagem.includes("{\"furbottext\": \"andarsul\"}") and direçãoAtual == 2:
+                lerCarta = False
+                andarReto = True
+            elif leituraImagem.includes("{\"furbottext\": \"andarleste\"}") and direçãoAtual == 3:
+                lerCarta = False
+                andarReto = True
+            else:
+                basic.pause(120)
+                basic.clear_screen()
+                basic.show_leds("""
                             # # . # #
                             . . . . .
                             . # # # .
                             # # . # #
                             # . . . #
-            """)
-            basic.pause(120)
-            basic.show_leds("""
+                """)
+                basic.pause(120)
+                basic.show_leds("""
                             # # . # #
                             # . . . .
                             . # # # .
                             # # . # #
                             # . . . #
-            """)
-            basic.pause(120)
-            basic.show_leds("""
+                """)
+                basic.pause(120)
+                basic.show_leds("""
                             # # . # #
                             . . . . .
                             # # # # .
                             # # . # #
                             # . . . #
-            """)
-def algoritmo_falhas():
-    if leituraImagem.length > 10:
-        if leituraImagem.includes("q" or "s" or "u"):
-            viraresquerda = True
-        elif leituraImagem.includes("i" or "t"):       
-            virardireita = True
-    elif leituraImagem.includes("and"):
-        tireiAndar = leituraImagem.replace("andar", None)
-        if leituraImagem.includes("u" or ""):
-            andarsul = True
-    elif leituraImagem.includes("vir"):
-        if leituraImagem.includes("q" or "s" or "u"):
-            viraresquerda = True
-        elif leituraImagem.includes("i" or "t"):
-            virardireita = True
+                """)
 
+def algoritmo_falhas():
+    leituraImagem = serial.read_string()
+    leituraImagem = leituraImagem.replace("{\"FurbotText\": ", "")
+    leituraImagem = leituraImagem.replace("}", "")
+    leituraImagem = leituraImagem.replace("\"", "")
+    leituraImagem = leituraImagem.replace("\"", "")
+    leituraImagem = leituraImagem.to_lower_case()
 
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let leituraImagem = ""
 let valoresIRs = 0
 let lerCarta = false
-let mensagemFoto = "{\"FurbotText\": \"TirarFoto\"}"
+let mensagemFoto = "{\"FurbotText\": \"tirar foto\"}"
 let direçãoAtual = 0
 // (N=0, O=1, S=2, L=3), VIRARDIREITA = +1, VIRARESQUERDA = -1;
 let andarReto = true
@@ -12,32 +12,12 @@ input.onSound(DetectedSound.Loud, function on_loud_sound() {
     let lerCarta = true
 })
 function algoritmo_falhas() {
-    let viraresquerda: boolean;
-    let virardireita: boolean;
-    let tireiAndar: string;
-    let andarsul: boolean;
-    if (leituraImagem.length > 10) {
-        if (leituraImagem.includes("q" || "s" || "u")) {
-            viraresquerda = true
-        } else if (leituraImagem.includes("i" || "t")) {
-            virardireita = true
-        }
-        
-    } else if (leituraImagem.includes("and")) {
-        tireiAndar = leituraImagem.replace("andar", null)
-        if (leituraImagem.includes("u" || "")) {
-            andarsul = true
-        }
-        
-    } else if (leituraImagem.includes("vir")) {
-        if (leituraImagem.includes("q" || "s" || "u")) {
-            viraresquerda = true
-        } else if (leituraImagem.includes("i" || "t")) {
-            virardireita = true
-        }
-        
-    }
-    
+    let leituraImagem = serial.readString()
+    leituraImagem = leituraImagem.replace("{\"FurbotText\": ", "")
+    leituraImagem = leituraImagem.replace("}", "")
+    leituraImagem = leituraImagem.replace("\"", "")
+    leituraImagem = leituraImagem.replace("\"", "")
+    leituraImagem = leituraImagem.toLowerCase()
 }
 
 basic.forever(function on_forever() {
@@ -84,76 +64,76 @@ basic.forever(function on_forever() {
             basic.pause(1640)
             leituraImagem = serial.readString()
             leituraImagem.toLowerCase()
-        }
-        
-        if (leituraImagem.includes("{\"furbottext\": \"virardireita\"}")) {
-            lerCarta = false
-            turtleBit.run(DIR.Run_back, 55)
-            basic.pause(280)
-            turtleBit.run(DIR.Turn_Right, 55)
-            direçãoAtual += 1
-            if (direçãoAtual > 3) {
-                direçãoAtual = 0
-            }
-            
-            basic.pause(1062)
-            turtleBit.run(DIR.Run_forward, 55)
-            basic.pause(280)
-            turtleBit.state(MotorState.stop)
-            lerCarta = true
-        } else if (leituraImagem.includes("{\"furbottext\": \"viraresquerda\"}")) {
-            lerCarta = false
-            turtleBit.run(DIR.Run_back, 55)
-            basic.pause(280)
-            turtleBit.run(DIR.Turn_Left, 55)
-            direçãoAtual += -1
-            if (direçãoAtual < 0) {
-                direçãoAtual = 3
-            }
-            
-            basic.pause(1145)
-            turtleBit.run(DIR.Run_forward, 55)
-            basic.pause(280)
-            turtleBit.state(MotorState.stop)
-            lerCarta = true
-        } else if (leituraImagem.includes("{\"furbottext\": \"andarnorte\"}") && direçãoAtual == 0) {
-            lerCarta = false
-            andarReto = true
-        } else if (leituraImagem.includes("{\"furbottext\": \"andaroeste\"}") && direçãoAtual == 1) {
-            lerCarta = false
-            andarReto = true
-        } else if (leituraImagem.includes("{\"furbottext\": \"andarsul\"}") && direçãoAtual == 2) {
-            lerCarta = false
-            andarReto = true
-        } else if (leituraImagem.includes("{\"furbottext\": \"andarleste\"}") && direçãoAtual == 3) {
-            lerCarta = false
-            andarReto = true
-        } else {
-            basic.pause(120)
-            basic.clearScreen()
-            basic.showLeds(`
+            if (leituraImagem.includes("{\"furbottext\": \"virardireita\"}")) {
+                lerCarta = false
+                turtleBit.run(DIR.Run_back, 55)
+                basic.pause(280)
+                turtleBit.run(DIR.Turn_Right, 55)
+                direçãoAtual += 1
+                if (direçãoAtual > 3) {
+                    direçãoAtual = 0
+                }
+                
+                basic.pause(1062)
+                turtleBit.run(DIR.Run_forward, 55)
+                basic.pause(280)
+                turtleBit.state(MotorState.stop)
+                lerCarta = true
+            } else if (leituraImagem.includes("{\"furbottext\": \"viraresquerda\"}")) {
+                lerCarta = false
+                turtleBit.run(DIR.Run_back, 55)
+                basic.pause(280)
+                turtleBit.run(DIR.Turn_Left, 55)
+                direçãoAtual += -1
+                if (direçãoAtual < 0) {
+                    direçãoAtual = 3
+                }
+                
+                basic.pause(1145)
+                turtleBit.run(DIR.Run_forward, 55)
+                basic.pause(280)
+                turtleBit.state(MotorState.stop)
+                lerCarta = true
+            } else if (leituraImagem.includes("{\"furbottext\": \"andarnorte\"}") && direçãoAtual == 0) {
+                lerCarta = false
+                andarReto = true
+            } else if (leituraImagem.includes("{\"furbottext\": \"andaroeste\"}") && direçãoAtual == 1) {
+                lerCarta = false
+                andarReto = true
+            } else if (leituraImagem.includes("{\"furbottext\": \"andarsul\"}") && direçãoAtual == 2) {
+                lerCarta = false
+                andarReto = true
+            } else if (leituraImagem.includes("{\"furbottext\": \"andarleste\"}") && direçãoAtual == 3) {
+                lerCarta = false
+                andarReto = true
+            } else {
+                basic.pause(120)
+                basic.clearScreen()
+                basic.showLeds(`
                             # # . # #
                             . . . . .
                             . # # # .
                             # # . # #
                             # . . . #
-            `)
-            basic.pause(120)
-            basic.showLeds(`
+                `)
+                basic.pause(120)
+                basic.showLeds(`
                             # # . # #
                             # . . . .
                             . # # # .
                             # # . # #
                             # . . . #
-            `)
-            basic.pause(120)
-            basic.showLeds(`
+                `)
+                basic.pause(120)
+                basic.showLeds(`
                             # # . # #
                             . . . . .
                             # # # # .
                             # # . # #
                             # . . . #
-            `)
+                `)
+            }
+            
         }
         
     }
